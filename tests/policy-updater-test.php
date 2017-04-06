ad51aaca90054e7507e1836603731b1d236469d0
bridger <bdysonsmith@gmail.com>
2017-04-06 16:28:22 -0400

    add a policy-updater-test.php script

---
+++ tests/policy-updater-test.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * User: bridger
+ * Date: 4/6/17
+ * Time: 10:09 AM
+ *
+ * Test updating a POLICY datastream
+ */
+
+/**
+ * $users = an imploded array of user names that will be passed to the XSL
+ * stylesheet. The implosion converts the array to a datatype the stylesheet
+ * can use.
+ */
+$users = implode(",", array("thesis_manager", "manager", "admin", "fedoraAdmin"));
+
+/**
+ * $resources = an imploded array of DSIDs that will be passed to the XSL
+ * stylesheet. The implosion converst the array to a datatype the stylesheet
+ * can use.
+ */
+$resources = implode(",", array("PDF", "FULL_TEXT", "SUPPL_0"));
+
+// add our stylesheet
+$xsl = new DOMDocument;
+$xsl->load('../xsl/policy-updater.xsl');
+
+// add our POLICY datatream
+$xml = new DOMDocument;
+$xml->load('./resources/xacml-policy-with-embargo.xml');
+
+// instantiate a new XSLTProcessor
+$proc = new XSLTProcessor();
+$proc->importStylesheet($xsl);
+$proc->setParameter('', 'users', $users);
+

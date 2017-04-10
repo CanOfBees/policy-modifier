97b17948c796e78ed50bd08d2474975591abd66b
bridger <bdysonsmith@gmail.com>
2017-04-10 16:16:33 -0400

    add policy-add-test php script

---
+++ tests/policy-adder-test.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * User: bridger
+ * Date: 4/6/17
+ * Time: 10:09 AM
+ *
+ * Test updating a POLICY datastream
+ */
+
+/**
+ * $users = an imploded array of user names that will be passed to the XSL
+ * stylesheet. The implosion converts the array to a datatype the stylesheet
+ * can use.
+ */
+$users = implode(",", array("thesis_manager", "manager", "admin", "fedoraAdmin", "derbyAdmin"));
+
+/**
+ * $resources = an imploded array of DSIDs that will be passed to the XSL
+ * stylesheet. The implosion converst the array to a datatype the stylesheet
+ * can use.
+ */
+$dsids = implode(",", array("PDF", "FULL_TEXT", "SUPPL_0", "FOO_TEXT"));
+
+// get the current working directory
+$current_directory = getcwd();
+
+// add our stylesheet
+$xsl = new DOMDocument;
+$xsl->load('../xsl/policy-adder.xsl');
+
+// add our POLICY datatream
+$xml = new DOMDocument;
+$xml->load('./resources/xacml-policy-blank.xml');
+
+// instantiate a new XSLTProcessor
+$proc = new XSLTProcessor();
+$proc->importStylesheet($xsl);
+// include parameters for the stylesheet
+$proc->setParameter('', 'users', $users);
+$proc->setParameter('', 'dsids', $dsids);
+
+$output_file_path = "file://{$current_directory}/output/policy-adder-results.xml";
+
+// output the result of the stylesheet
+$proc->transformToUri($xml, $output_file_path);
